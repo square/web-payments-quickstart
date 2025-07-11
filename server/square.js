73fc7995457e3c75f26b3f5bb269a03fb854c5a5
Nathan Wilmes <nwilmes@squareup.com>
2025-07-11 15:56:36 -0700

    lintfix

--- server/square.js
+++ server/square.js
@@ -3,7 +3,9 @@ const { SquareError, SquareClient, SquareEnvironment } = require('square');
 const { isProduction, SQUARE_ACCESS_TOKEN } = require('./config');
 
 const client = new SquareClient({
-  environment: isProduction ? SquareEnvironment.Production : SquareEnvironment.Sandbox,
+  environment: isProduction
+    ? SquareEnvironment.Production
+    : SquareEnvironment.Sandbox,
   token: SQUARE_ACCESS_TOKEN,
 });
 
